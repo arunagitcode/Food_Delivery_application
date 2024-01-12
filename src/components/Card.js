ee690af80f91218905f7aa441ad7bbbcff8c6572
arunagitcode <arunagitcode@gmail.com>
2024-01-12 15:44:46 +0530

    card design fixed

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,6 @@
 import { CARD_URL } from "../utils/Logos";
 
 const Card = (resData) => {
-
   const {
     cloudinaryImageId,
     avgRating,
@@ -16,16 +15,19 @@ const Card = (resData) => {
   return (
     <div className="card_container">
       <div className="card_image_container">
-        <img alt="cards_images" src={CARD_URL+resData?.resData?.info?.cloudinaryImageId}></img>
+        <img
+          alt="cards_images"
+          src={CARD_URL + resData?.resData?.info?.cloudinaryImageId}
+        ></img>
       </div>
 
       <div className="card_text_container">
-        {name}
-        {cuisines}
-        {avgRating}
-        {totalRatingsString}
-        {costForTwo}
-        {isOpen}
+        <h4>{name}</h4>
+        <h5>
+          {avgRating} {totalRatingsString} {costForTwo}
+        </h5>
+        <h5> {cuisines}</h5>
+       
       </div>
     </div>
   );
