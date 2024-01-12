2344bd84cbd6090a4a84cc626fc0f0c78868de69
arunagitcode <arunagitcode@gmail.com>
2024-01-12 15:19:57 +0530

    final look

--- src/utils/Logos.js
+++ src/utils/Logos.js
@@ -1,2 +1,4 @@
-export const LOGO_URL = "https://assets.materialup.com/uploads/61d86780-be13-47fa-81a6-226aac22db27/preview.jpg"
-export const CARD_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_366/"
+export const LOGO_URL =
+  "https://images.creativemarket.com/0.1.0/ps/7262394/1820/1214/m1/fpnw/wm0/dining-restaurant-logo-design-inspiration-.jpg?1573169154&s=f5468f33601a341466e0fb5dc1da8c4d";
+export const CARD_URL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_366/";
