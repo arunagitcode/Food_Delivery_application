0e852e224ff2e6534e10c4d4ce4f67b750f39ec0
arunagitcode <arunagitcode@gmail.com>
2024-01-13 20:27:26 +0530

    impemented search functionality

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,6 +3,9 @@ import Card from "./Card";
 
 const Body = () => {
   const [initialdata, setInitialData] = useState([]);
+  const [filteredList, setFilteredList] = useState([]);
+
+  const [searchValue, setSearchValue] = useState("");
 
   useEffect(() => {
     fetchData();
@@ -20,17 +23,40 @@ const Body = () => {
     setInitialData(
       json.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
     );
+    setFilteredList(
+      json.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
+  const handleTopRatedRest = () => {
+    const TopRatedRest = initialdata.filter((res) => res?.info.avgRating > 4.5);
+    setInitialData(TopRatedRest);
+  };
+
+  const handleSearchValue = () => {
+    const filteredData = initialdata.filter((res) =>
+      res?.info.name.toLowerCase().includes(searchValue.toLowerCase())
+    );
+    setFilteredList(filteredData);
   };
   return (
     <div className="body_container">
       <div className="search_container">
         <div className="input_field">
-          <input type="text" placeholder="search for restaurants and food" />
-          <button>search</button>
+          <input
+            type="text"
+            placeholder="search for restaurants and food"
+            value={searchValue}
+            onChange={(e) => {
+              setSearchValue(e.target.value);
+            }}
+          />
+          <button onClick={handleSearchValue}>search</button>
         </div>
+        <button onClick={handleTopRatedRest}>Top Rated Restaurants</button>
       </div>
       <div className="cards">
-        {initialdata.map((item) => (
+        {filteredList.map((item) => (
           <Card key={item.info.id} resData={item} />
         ))}
       </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import { LOGO_URL } from "../utils/Logos";
 const Header = () => {
+  const [loginbtn, setLoginBtn] = useState("Login");
   return (
     <div className="header_container">
       <div className="logo">
@@ -8,11 +10,21 @@ const Header = () => {
       <div className="text_container">SPICY FOOD</div>
       <div className="nav_container">
         <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Order</li>
-            <li>Contact Us</li>
-            <li>Login</li>
+          <li>Home</li>
+          <li>About</li>
+          <li>Order</li>
+          <li>Contact Us</li>
+          <li className="login_btn">
+            <button
+              onClick={() => {
+                loginbtn === "Login"
+                  ? setLoginBtn("LogOut")
+                  : setLoginBtn("Login");
+              }}
+            >
+              {loginbtn}
+            </button>
+          </li>
         </ul>
       </div>
     </div>
